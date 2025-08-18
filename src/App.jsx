8c8272fea261f9852823ab4a5cee90debca3d05d
vvvvvv eeeeee <hiddenether@yahoo.com>
2025-08-18 00:08:21 -0400

    initial skeleton

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const STORAGE_KEY = 'taskList;'
+
+function Input({ tasks, setTasks }) {
+  
+
+  return (
+    
+  );
+}
+
+function TaskItem({ task, tasks, setTasks }) {
+  
+
+  return (
+    
+  );
+}
+
+function TaskList({ tasks, setTasks }) {
+  
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  );
+}
